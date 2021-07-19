0aa92130712adf3ed1d898a851809c9abc1bd574
l46kok <l47kok@gmail.com>
2021-07-19 14:21:46 -0700

    Handle forfeited game

--- ReplayParser/Validators/FateGameValidator.cs
+++ ReplayParser/Validators/FateGameValidator.cs
@@ -13,8 +13,6 @@ namespace FateReplayParser.Validators
         
         public static bool IsFateGameValid(ReplayData fateReplayData)
         {
-            if (!IsGameDurationValid(fateReplayData))
-                return false;
             if (fateReplayData.GameMode != GameMode.DM)
             {
                 logger.Trace("Skipping stats insert: Non DeathMatch game.");
@@ -33,19 +31,30 @@ namespace FateReplayParser.Validators
                 return false;
             }
 
-            int maxRoundScore = Math.Max(fateReplayData.TeamOneVictoryCount, fateReplayData.TeamTwoVictoryCount);
-            if (maxRoundScore < 10)
-            {
-                logger.Trace($"Skipping stats insert: Max round score is less than 10 (MaxRoundScore: {maxRoundScore})");
-                return false;
-            }
-
             if (fateReplayData.PlayerInfoList.Count(x => x.Team == 0 && !x.IsObserver) !=
                 fateReplayData.PlayerInfoList.Count(x => x.Team == 1 && !x.IsObserver))
             {
                 logger.Trace($"Skipping stats insert: Uneven Teams");
                 return false;
             }
+
+            if (!fateReplayData.IsForfeitedGame)
+            {
+                if (!IsGameDurationValid(fateReplayData))
+                    return false;
+
+                int maxRoundScore = Math.Max(fateReplayData.TeamOneVictoryCount, fateReplayData.TeamTwoVictoryCount);
+                if (maxRoundScore < 10)
+                {
+                    logger.Trace($"Skipping stats insert: Max round score is less than 10 (MaxRoundScore: {maxRoundScore})");
+                    return false;
+                }
+            }
+            else
+            {
+                logger.Trace("Forfeited game. Bypassing game duration and max round score validation");
+            }
+
             return true;
         }
         //Length must be 20 minutes or more
